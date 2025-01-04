d5b662cc47351856f6c182f1f9dba341769a881b
Yuri Baumgartner <yuribaumgartner@ice.ufjf.br>
2025-01-04 00:29:38 -0300

    Remove unused files and update dependencies

--- App.js
+++ App.js
@@ -1,9 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Routes from './src/Routes/Root.routes';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Updates from 'expo-updates';
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  const checkUpdates = async () => {
+    try {
+      const { isAvailable } = await Updates.checkForUpdateAsync(); // Check for updates and return information about the update
+
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync(); // Fetch the update and cache it until the app is restarted
+        await Updates.reloadAsync(); // Reload the app to apply the update
+      }
+    } catch (error) {
+      console.error('Failed to check for updates', error);
+    } finally {
+      setIsReady(true);
+    }
+  };
+
+  useEffect(() => {
+    if (!__DEV__) {
+      checkUpdates();
+    } else setIsReady(true);
+  }, []);
+
+  if (!isReady) return null;
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
--- src/Routes/Root.routes.jsx
+++ src/Routes/Root.routes.jsx
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { PublicRoutes } from './Public.routes';
 import Adsbanner from '../componentes/Adsbanner';
+import * as SplashScreen from 'expo-splash-screen';
 
-
+SplashScreen.hideAsync();
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <PublicRoutes />
-      <Adsbanner/>
+      <Adsbanner />
     </NavigationContainer>
   );
 }
